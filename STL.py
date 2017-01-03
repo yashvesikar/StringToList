1840a251cb86815d6ca4f3dc157cdc0715413043
yashvesikar <yashvesikar@gmail.com>
2017-01-03 13:43:13 -0500

    Added comments to code

--- STL.py
+++ STL.py
@@ -1,14 +1,19 @@
+#Given string that we would like to split up into its component words
 string = 'shoeboxmynameisyashIlikeCheeseandfish'
 
-D = ['he', 'hello','my','name','is','yash', "house",'shoebox','shoe','box',]
+#Assume that some dictionary D is given that contains a large list of words including the words from within the string
+D = ['he', 'hello','my','name','is','yash', "house",'shoebox','shoe','box','ox']
 
 empty_string = ''
 
 
 def p(string,dictionary):
+    #This function finds the words within some given string given a 'dictionary' and a string
     if string == '':
         return string
     
+    #This algorithm will find the longest possible word. 
+    #i.e. shoe and shoebox are both in the tring and dictionary, however shoebox will be in the output
     i = 0
     words = []    
     while i <len(string):
